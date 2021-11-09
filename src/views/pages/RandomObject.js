30b2df3ca3bfb6bb19fb4e487f6d5ef48002450b
Alpakash <akash.soedamah@gmail.com>
2021-11-09 12:16:40 +0100

    removed 3d cube object, legacy cdn script, added attributes to 3d 'circle' element

--- src/views/pages/RandomObject.js
+++ src/views/pages/RandomObject.js
@@ -4,24 +4,20 @@ let RandomObject = {
 		document.getElementById("title").innerHTML = null;
 		document.getElementById("subtitle").innerHTML = null;
 		document.getElementById("description").innerHTML = null;
+
 		let view = /*html*/ `
-					<model-viewer
-						src="./assets/projectName.gltf"
-						alt="Random 3d block"
-						auto-rotate auto-rotate-delay=0
-						progress=0
-					></model-viewer>
+			<model-viewer
+				src="./assets/circle.glb"
+				alt="A 3D model of a circle"
+				auto-rotate
+				auto-rotate-delay=0
+				rotation-per-second="60deg"
+				interaction-prompt="none"
+				camera-controls=""
+				background-color="#455A64"
+			></model-viewer>
+		`;
 
-					<model-viewer
-						src="./assets/circle.glb"
-						progress=0
-						alt="A 3D model of a circle"
-						auto-rotate=""
-						camera-controls=""
-						background-color="#455A64"
-					></model-viewer>
-
-					`;
 		return view;
 	}
 };
