3fc8151bbb52b5c7c15006e745c3a12d543679be
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-05 12:52:38 +0100

    Switched from expect to assert, TDD > BDD

--- test/appTest.js
+++ test/appTest.js
@@ -1,27 +1,31 @@
-const expect = require("chai").expect;
+const assert = require("chai").assert;
 // let app = require("../src/app").router;
 
 
-// Learning the expect BDD chai assertion style
+// Learning the assert TDD chai assertion style
 describe("App", function() {
 	const test = "abc",
 		imTrue = true,
 		imUndefined = undefined,
 		imArray = [1, 2, 3];
 
-	it("Expect test variable to include abc", function() {
-		expect(test).to.have.length(3);
+	it("Test variable should have a length of 3", function() {
+		assert.lengthOf(test, 3);
 	});
 
-	it("Expect value to be true" , () => {
-		expect(imTrue).to.be.true;
+	it("Value should return true", () => {
+		assert.isTrue(imTrue, "Should return true");
+	});
+
+	it("Value should be undefined" , () => {
+		assert.isUndefined(imUndefined, "Should return undefined");
 	})
 
-	it("Expect value to be undefined" , () => {
-		expect(imUndefined).to.be.undefined;
+	it("Value should be an Array" , () => {
+		assert.isArray(imArray, "Should return Array");
 	})
 
-	it("Expect value to be Array" , () => {
-		expect(imArray).to.be.a("array");
+	it("Check if assertion is a deepEqual", () => {
+		assert.deepEqual({ tea: "green" }, { tea: "green" });
 	})
 });
