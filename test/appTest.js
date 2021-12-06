22a041ce95dbe1c086d7b8f332a1efe8e3c61832
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-06 15:55:32 +0100

    remove not specified plugin from babelrc

--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,5 @@
 const assert = require("chai").assert;
-const router = require("../src/app").router;
+// const router = require("../src/app").router;
 
 // Learning the assert TDD chai assertion style
 describe("App", function() {
@@ -9,11 +9,11 @@ describe("App", function() {
 		imArray = [1, 2, 3];
 
 
-	it("Let this test fail", () => {
-		console.log(router);
+	// it("Let this test fail", () => {
+	// 	console.log(router);
 
-		assert.isFunction("", "message");
-	})
+	// 	assert.isFunction("", "message");
+	// })
 
 	it("Test variable should have a length of 3", () => {
 		assert.lengthOf(test, 3);
