188b4f2d9bd4610ddfb825acb80c26b8d616e3b9
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-06 15:09:24 +0100

    fix mocha errors for testing ES6 modules and make the window object available with jsdom-global

--- src/views/pages/Home.js
+++ src/views/pages/Home.js
@@ -1,3 +1,5 @@
+import "regenerator-runtime/runtime";
+
 const getShow = async () => {
 	try {
 		const response = await fetch(
--- test/appTest.js
+++ test/appTest.js
@@ -1,5 +1,5 @@
 const assert = require("chai").assert;
-const app = require("../src/app");
+const router = require("../src/app").router;
 
 // Learning the assert TDD chai assertion style
 describe("App", function() {
@@ -8,29 +8,30 @@ describe("App", function() {
 		imUndefined = undefined,
 		imArray = [1, 2, 3];
 
-	describe("Let this test fail", () => {
-		console.log(app.routes);
 
-		assert.isFunction("", "[message]");
+	it("Let this test fail", () => {
+		console.log(router);
+
+		assert.isFunction("", "message");
 	})
 
-	describe("Test variable should have a length of 3", () => {
+	it("Test variable should have a length of 3", () => {
 		assert.lengthOf(test, 3);
 	});
 
-	describe("Value should return true", () => {
+	it("Value should return true", () => {
 		assert.isTrue(imTrue, "Should return true");
 	});
 
-	describe("Value should be undefined" , () => {
+	it("Value should be undefined" , () => {
 		assert.isUndefined(imUndefined, "Should return undefined");
 	})
 
-	describe("Value should be an Array" , () => {
+	it("Value should be an Array" , () => {
 		assert.isArray(imArray, "Should return Array");
 	})
 
-	describe("Check if assertion is a deepEqual", () => {
+	it("Check if assertion is a deepEqual", () => {
 		assert.deepEqual({ tea: "green" }, { tea: "green" });
 	})
 });
