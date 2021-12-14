5ae450a351fdb1862c9348ef738ddc811ac57a11
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-14 17:27:00 +0100

    got to mock my fetch request

--- test/showTest.js
+++ test/showTest.js
@@ -1,24 +1,16 @@
 const assert = require("chai").assert;
-// import Show from "../src/views/pages/Show";
+import Show from "../src/views/pages/Show";
+import fetch from "cross-fetch";
 
 // I want to test this Show page. Tomorrows challenge it is.
+// now reading https://swizec.com/blog/mocking-and-testing-fetch-requests-with-jest <- got to moch my fetch request
 describe("The show page", () => {
 	it("The list with episodes should contain 3 list items", () => {
-			// const getEpisodes = async (id) => {
-			// 	try {
-			// 		const response = await fetch(
-			// 			`https://api.tvmaze.com/shows/${id}/episodes`
-			// 		);
-			// 		const json = await response.json();
-
-			// 		return json;
-			// 	} catch (err) {
-			// 		console.log("Error getting documents", err);
-			// 	}
-			// };
-
-			// console.log(getEpisodes(1955));
-
-		// console.log(Show.render);
+			const getEpisodes = async (id) => {
+				fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
+					.then((response) => {return response.json()})
+					.then((data) => {return data})
+					.catch((error) => console.log("Error getting documents", error));
+			};
 	})
 });
