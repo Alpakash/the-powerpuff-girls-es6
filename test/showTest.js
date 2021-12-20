b7b4b8e06666da046dfd521f7a67f984323ffe56
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-20 10:04:33 +0100

    Installed jest to dependencies

--- test/showTest.js
+++ test/showTest.js
@@ -2,8 +2,7 @@ const assert = require("chai").assert;
 import Show from "../src/views/pages/Show";
 import fetch from "cross-fetch";
 
-// I want to test this Show page. Tomorrows challenge it is.
-// now reading https://swizec.com/blog/mocking-and-testing-fetch-requests-with-jest <- got to moch my fetch request
+// now reading https://swizec.com/blog/mocking-and-testing-fetch-requests-with-jest <- got to mock my fetch request
 describe("The show page", () => {
 	it("The list with episodes should contain 3 list items", () => {
 			const getEpisodes = async (id) => {
